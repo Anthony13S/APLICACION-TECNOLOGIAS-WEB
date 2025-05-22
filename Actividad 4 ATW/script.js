b8155e4c42abc58d13ab84f42620f615fafc18c4
Anthony13S <150402671+Anthony13S@users.noreply.github.com>
2025-05-21 21:22:34 -0500

    Update script.js

--- Actividad 4 ATW/script.js
+++ Actividad 4 ATW/script.js
@@ -17,11 +17,4 @@ mensajeElemento.className = "resultado";
 if (notaFinal >= 14) {
     mensajeElemento.textContent = "Resultado: Aprobado directamente.";
     mensajeElemento.classList.add("aprobado");
-} else if (notaFinal >= 10) {
-    mensajeElemento.textContent = "Resultado: Debe ir a recuperación.";
-    mensajeElemento.classList.add("recuperacion");
-} else {
-    mensajeElemento.textContent = "Resultado: Pierde la materia.";
-    mensajeElemento.classList.add("perdido");
-}
 
