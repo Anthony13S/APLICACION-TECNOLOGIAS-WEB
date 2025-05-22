16e85d9fe9608ee242252106a2be495249ff2b11
Anthony13S <150402671+Anthony13S@users.noreply.github.com>
2025-05-22 13:26:32 -0500

    Update script.js

--- Actividad 4 ATW/script.js
+++ Actividad 4 ATW/script.js
@@ -10,15 +10,4 @@ const notaFinal = (parcial1 * 0.3) + (parcial2 * 0.3) + (proyectoFinal * 0.4);
 const notaElemento = document.getElementById("notaFinal");
 const mensajeElemento = document.getElementById("mensaje");
 
-// Evaluación del resultado
-if (notaFinal >= 14) {
-    mensajeElemento.textContent = "Resultado: Aprobado directamente.";
-    mensajeElemento.classList.add("aprobado");
-} else if (notaFinal >= 10) {
-    mensajeElemento.textContent = "Resultado: Debe ir a recuperación.";
-    mensajeElemento.classList.add("recuperacion");
-} else {
-    mensajeElemento.textContent = "Resultado: Pierde la materia.";
-    mensajeElemento.classList.add("perdido");
-}
 
