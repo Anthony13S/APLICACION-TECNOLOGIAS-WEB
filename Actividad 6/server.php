e2a04949ffc1bc9c5334636bf2e80f099c9262e6
Anthony13S <150402671+Anthony13S@users.noreply.github.com>
2025-07-06 00:20:42 -0500

    Update server.php

--- Actividad 6/server.php
+++ Actividad 6/server.php
@@ -1,8 +1,4 @@
-<?php
-echo "<!DOCTYPE html><html lang='es'><head><meta charset='UTF-8'><title>Resultados</title>";
-echo "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet'>";
-echo "</head><body class='bg-dark text-light container py-5'>";
-echo "<h2 class='mb-4'>Datos Recibidos</h2><ul class='list-group'>";
+
 
 foreach ($_POST as $campo => $valor) {
     if (is_array($valor)) {
