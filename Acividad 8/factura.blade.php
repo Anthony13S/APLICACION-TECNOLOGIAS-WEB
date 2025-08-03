a795a5f0a150e9dc0faa1c7479cc51600c342a4e
Anthony13S <150402671+Anthony13S@users.noreply.github.com>
2025-08-03 16:27:00 -0500

    Update factura.blade.php

--- Acividad 8/factura.blade.php
+++ Acividad 8/factura.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Factura</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap 5 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
 
-<div class="container py-5">
-
-    <h2 class="text-center mb-4">Formulario de Factura</h2>
 
     <!-- FORMULARIO -->
     <div class="card shadow">
@@ -85,5 +72,6 @@
 
 </body>
 </html>
+
 
 
